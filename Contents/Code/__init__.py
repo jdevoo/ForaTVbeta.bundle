c1e114bf732b1af63d74f5195b10e92fc68b1dd0
JP de Vooght <jp@vooght.de>
2010-04-10 18:16:42 +0200

    On branch master Changes to be committed:
    modified:   Contents/Code/__init__.py
    modified:   Contents/Info.plist
    
    Incremented plug-in name
    Added views and comments to most recent, search and week's most... views
    Removed week's most rated
    Replaced blue folder with icon-default.png

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,7 +3,7 @@ from PMS.Objects import *
 from PMS.Shortcuts import *
 import urllib
 
-FTV_PREFIX = '/video/foratvbeta'
+FTV_PREFIX = '/video/foratvbeta1'
 FTV_ROOT   = 'http://fora.tv'
 FTV_TOPICS = ['Economy', 'Environment', 'Politics', 'Science', 'Technology', 'Culture']
 CACHE_INTERVAL	= 3600 * 6
@@ -13,8 +13,9 @@ def Start():
   Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
   Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
   MediaContainer.title1 = 'FORA.tv'
-  MediaContainer.content = 'Items'
+  MediaContainer.content = 'items'
   MediaContainer.art = R('art-default.png')
+  DirectoryItem.thumb = R('icon-default.png')
   HTTP.SetCacheTime(CACHE_INTERVAL)
 
 def UpdateCache():
@@ -23,11 +24,10 @@ def UpdateCache():
 def MainMenu():
   dir = MediaContainer()
   dir.Append(Function(DirectoryItem(FeaturedMenu, title="Featured")))
-  dir.Append(Function(DirectoryItem(MostStarMenu, title="Week's Most Watched"), choice="most_watched_content"))
-  dir.Append(Function(DirectoryItem(MostStarMenu, title="Week's Most Commented"), choice="most_commented_content"))
-  dir.Append(Function(DirectoryItem(MostStarMenu, title="Week's Most Rated"), choice="most_rated_content"))
+  dir.Append(Function(DirectoryItem(MostStarMenu, title="Week's Most Watched"), choice='views'))
+  dir.Append(Function(DirectoryItem(MostStarMenu, title="Week's Most Commented"), choice='comments'))
   dir.Append(Function(DirectoryItem(TopicMenu, title="By Topic")))
-  dir.Append(Function(SearchDirectoryItem(SearchMenu, title='Search FORA.tv', prompt='Search FORA.tv')))
+  dir.Append(Function(SearchDirectoryItem(SearchMenu, thumb=R('icon-default.png'), title='Search FORA.tv', prompt='Search FORA.tv')))
   return dir
 
 def FeaturedMenu(sender, choice=''):
@@ -55,25 +55,34 @@ def TopicMenu(sender, choice=''):
       key = e.xpath('.//div[@class="featured_title"]/a')[0].get('href')+'#full_program'
       thumb = e.xpath('.//div[@class="cropped_image"]')[0].get('style')
       subtitle = e.xpath('.//div[@class="l_partner"]/a')[0].text
-      dir.Append(WebVideoItem(FTV_ROOT+key, title, subtitle, thumb=FTV_ROOT+thumb[thumb.find('(')+1:thumb.find(')')]))
+      summary = 'Views: %s\nComments: %s' % (e.xpath('.//span[@class="views"]')[0].text, e.xpath('.//span[@class="views"]')[1].text)
+      dir.Append(WebVideoItem(FTV_ROOT+key, title, subtitle, summary, thumb=FTV_ROOT+thumb[thumb.find('(')+1:thumb.find(')')]))
   return dir
 
 def MostStarMenu(sender, choice):
-  dir = MediaContainer(viewGroup='List', title2=' '.join(choice.split('_')[0:2]).title())
-  for e in XML.ElementFromURL(FTV_ROOT, True).xpath('//div[@id="%s"]/dl/div[@class="ol"]' % choice):
-    title = e.xpath('.//dd/a')[0].text
-    key = e.xpath('.//dd/a')[0].get('href')+'#full_program'
-    dir.Append(WebVideoItem(FTV_ROOT+key, title))
+  dir = MediaContainer(viewGroup='InfoList', title2="Week's Most Watched" if choice == 'views' else "Week's Most Commented")
+  res = []
+  for topic in map(str.lower, FTV_TOPICS):
+    for e in XML.ElementFromURL('%s/topic/%s/all?sort=%s' % (FTV_ROOT, topic, choice), True).xpath('//div[@class="featured_bit"]'):
+      title = e.xpath('.//div[@class="featured_title"]/a')[0].text
+      key = e.xpath('.//div[@class="featured_title"]/a')[0].get('href')+'#full_program'
+      thumb = e.xpath('.//div[@class="cropped_image"]')[0].get('style')
+      subtitle = e.xpath('.//div[@class="l_partner"]/a')[0].text
+      c = int(e.xpath('.//span[@class="views"]')[0 if choice == 'views' else 1].text.replace(',', ''))
+      res += [(c, key, title, subtitle, thumb, topic)]
+  sres = sorted(res, key=lambda item: item[0])
+  for item in sres[0:10]:
+    dir.Append(WebVideoItem(FTV_ROOT+item[1], item[2], item[3], item[5].title()+'\n'+str(item[0])+(' Views' if choice == 'views' else ' Comments'), thumb=FTV_ROOT+item[4][item[4].find('(')+1:item[4].find(')')]))
   return dir
 
 def SearchMenu(sender, query):
   dir = MediaContainer(viewGroup='InfoList', title2=query)
-  for e in XML.ElementFromURL(FTV_ROOT+'/search_video?%s?per_page=50' % urllib.urlencode({'q':query}), True).xpath('//div[@class="clip_bit "]'):
+  for e in XML.ElementFromURL(FTV_ROOT+'/search_video?%s&per_page=50' % urllib.urlencode({'q':query}), True).xpath('//div[@class="clip_bit "]'):
     title = e.xpath('.//a[@class="clip_bit_title"]')[0].text
     key = e.xpath('.//a[@class="cropped_thumb"]')[0].get('href')+'#full_program'
     thumb = e.xpath('.//a[@class="cropped_thumb"]/img')[0].get('src')
     subtitle = e.xpath('.//div[@class="l_partner"]/a')[0].text
-    summary = e.xpath('.//div[@class="description_long"]')[0].text
+    summary = 'Views: %s\nComments: %s' % (e.xpath('.//span[@class="views"]')[0].text, e.xpath('.//span[@class="views"]')[1].text)
     dir.Append(WebVideoItem(FTV_ROOT+key, title, subtitle, summary, thumb=FTV_ROOT+thumb))
   return dir
 
